8aa5be7836cae9a29d8b1132251f8c1d5479a822
danilovalerio <danilo_vs@hotmail.com>
2022-09-13 18:09:49 -0300

    Primeiro teste com Roteiro

---
+++ app/src/test/java/br/com/alura/leilao/model/LeilaoTest.java
@@ -0,0 +1,18 @@
+package br.com.alura.leilao.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LeilaoTest {
+
+    @Test
+    public void getDescricao(){
+        // criar cenario de teste
+        // executar ação esperada
+        // testar resultado esperado
+
+    }
+
+}
+
